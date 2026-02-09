5d13a38d7cfbc0e2d8ba1aa2c76a6916cbd83525
Diamondbanana420 <benjamesbradshaw22@gmail.com>
2026-02-09 23:30:38 +1000

    V9.3: src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './V9.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './V9.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
